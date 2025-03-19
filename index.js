ae9c161c35b9acdd7cdedb9ed1185dea4b87d107
AMOB-Kahuna <balloabdulmujeeb42@gmail.com>
2025-03-19 14:48:51 +0100

    render the menus on the page using javscript

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 import menuArray from "/data.js"
 
+const menuContianer = document.querySelector("#menus-container");
+
+function renderMenu() {
+
+    const menuContent = menuArray.map( item => {
+        return `<section class="menu-item outer">
+            <div class="inner">
+                <p class="menu-img">${item.emoji}</p>
+                <div class="menu-details" id="menu-details">
+                    <h2 class="bold-text">${item.name}</h2>
+                    <p class="flavour">${item.ingredients.join(", ")}</p>
+                    <p class="price-text">$${item.price}</p>
+                </div>
+            </div>
+            <button class="add-to-cart-btn">+</button>
+        </section>`
+    })
+
+    menuContianer.innerHTML = menuContent.join("")
+}
+
+renderMenu()
+
